bd59daf08b3437a62ec186c3612bf78c394a83a6
Evolveye <stolarski.paw@gmail.com>
2020-09-04 22:36:06 +0200

    Init

---
+++ CactuJam-7/src/App.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import Web from "./components/web"
+
+import "./App.css"
+
+export default () => <Web />
---
+++ CactuJam-7/src/components/web.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import WebCell from "./WebCell"
+
+export default class Web extends React.Component {
+  render = () => <canvas class="game-web" />
+}
