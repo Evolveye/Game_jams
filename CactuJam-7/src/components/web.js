769e2b829a51ca5f705c39b2a8f4bd70b9deb83c
Evolveye <stolarski.paw@gmail.com>
2020-09-05 01:22:36 +0200

    WebCells drawing

--- CactuJam-7/src/components/web.js
+++ CactuJam-7/src/components/web.js
@@ -5,8 +5,6 @@ import WebCell from "./WebCell"
 export default class Web extends React.Component {
   width = window.innerWidth
   height = window.innerHeight
-  centerX = this.width / 2
-  centerY = this.height / 2
   /** @type {HTMLCanvasElement} */
   canvas = null
   /** @type {CanvasRenderingContext2D} */
@@ -15,14 +13,17 @@ export default class Web extends React.Component {
 
   /** @type {WebCell[][]} */
   level = [[]]
-  webCellSize = 9
+  webCellSize = 19
+  offsetLeft = 0
+  offsetTop = 0
 
-  temp = 0
+  webColor = `white`
 
   componentDidMount() {
     this.handleResize()
     this.setDefaults()
     this.startLoop()
+    window.game = this
   }
 
   startLoop() {
@@ -39,9 +40,6 @@ export default class Web extends React.Component {
     this.width = window.innerWidth
     this.height = window.innerHeight
 
-    this.centerX = this.width / 2
-    this.centerY = this.height / 2
-
     this.canvas.width = this.width
     this.canvas.height = this.height
   }
@@ -52,20 +50,76 @@ export default class Web extends React.Component {
   }
   setDefaults() {
     // % of tile creation
-    this.level = [ 10, 30, 60, 90, 60, 30, 10 ]
-      .map( (procent, _, { length } ) => Array.from( { length }, () => Math.random() > procent / 100 ? null : new WebCell() ) )
+    const procents = [ 10, 20, 30, 40, 50, 60, 80, 100, 100 ].reverse()
+    const center = Math.floor( procents.length / 2 )
+
+    this.level = Array.from( { length:procents.length }, (_, y) =>
+      Array.from( { length:procents.length }, (_, x) =>
+        Math.random() > procents[ Math.abs( center - y ) + Math.abs( center - x ) ] / 100 ? null : new WebCell()
+      )
+    )
+
+    this.level.forEach( (row, y) => row.forEach( (webCell, x) => {
+      if (!webCell) return
+      if (this.level[ y + 0 ]?.[ x - 1 ]) webCell.neighbours.left = true
+      if (this.level[ y - 1 ]?.[ x + 0 ]) webCell.neighbours.top = true
+      if (this.level[ y + 0 ]?.[ x + 1 ]) webCell.neighbours.right = true
+      if (this.level[ y + 1 ]?.[ x + 0 ]) webCell.neighbours.bottom = true
+    } ) )
   }
 
   logic = () => {
+    let longestRow = 0
 
+    this.level.forEach( row => row.length > longestRow && (longestRow = row.length) )
+
+    this.offsetLeft = this.width / 2 - longestRow * this.webCellSize / 2
+    this.offsetTop = this.height / 2 - this.level.length * this.webCellSize / 2
   }
   draw = () => {
-    const { ctx, centerX, centerY } = this
+    const { ctx, offsetLeft, offsetTop, webCellSize, webColor } = this
 
-    this.temp += 1
+    // Draw cells
+    this.level.forEach( (row, y) => row.forEach( (webCell, x) => {
+      if (!webCell) return
 
-    ctx.fillStyle = `red`
-    ctx.fillRect( centerX + this.temp, centerY, 10, 10 )
+      const { left, top, right, bottom } = webCell.neighbours
+
+      const offsetX = offsetLeft + x * webCellSize
+      const offsetY = offsetTop + y * webCellSize
+
+      ctx.beginPath()
+
+      ctx.moveTo( offsetX, offsetY + webCellSize / 2 )
+      ctx.lineTo( offsetX + webCellSize, offsetY + webCellSize / 2 )
+
+      ctx.moveTo( offsetX + webCellSize / 2, offsetY )
+      ctx.lineTo( offsetX + webCellSize / 2, offsetY + webCellSize )
+
+      ctx.strokeStyle = `#555`
+      ctx.stroke()
+
+      ctx.beginPath()
+
+      if (!left) {
+        ctx.moveTo( offsetX, offsetY )
+        ctx.lineTo( offsetX, offsetY + webCellSize )
+      }
+      if (!top) {
+        ctx.moveTo( offsetX, offsetY )
+        ctx.lineTo( offsetX + webCellSize, offsetY )
+      }
+      if (!right) {
+        ctx.moveTo( offsetX + webCellSize, offsetY )
+        ctx.lineTo( offsetX + webCellSize, offsetY + webCellSize )
+      }
+      if (!bottom) {
+        ctx.moveTo( offsetX, offsetY + webCellSize )
+        ctx.lineTo( offsetX + webCellSize, offsetY + webCellSize )
+      }
+      ctx.strokeStyle = `#fff`
+      ctx.stroke()
+    } ) )
   }
   render = () => <canvas ref={this.handleRef} className="game-web" />
 }
