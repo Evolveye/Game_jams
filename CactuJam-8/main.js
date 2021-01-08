e1c734f2b77038842a7b6d078fe6d502f921bc92
Evolveye <stolarski.paw@gmail.com>
2021-01-09 00:45:34 +0100

    First window

--- CactuJam-8/main.js
+++ CactuJam-8/main.js
@@ -1,1 +1,17 @@
-console.log( `Hello world` )
+const { app, BrowserWindow } = require( `electron` )
+
+app.whenReady().then( main )
+
+function main() {
+  const win = new BrowserWindow( {
+    width: 1000,
+    height: 1000,
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+    }
+  } )
+
+  win.loadFile( `./index.html` )
+}
