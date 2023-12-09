5e1a3bdc16ac51883e2235b261fc042f023c8a5d
evolveye <stolarski.paw@gmail.com>
2023-12-09 16:25:34 +0100

    CactuJam 9 output

--- CactuJam-9/gatsby-config.js
+++ CactuJam-9/gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: `/Game_jams/CactuJam-11/public/`,
   siteMetadata: {
     title: "CactuJam 9",
   },
---
+++ CactuJam-9/public/component---cache-caches-gatsby-plugin-offline-app-shell-js-e24185520deeb15bc4cd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcactu_jam_9=self.webpackChunkcactu_jam_9||[]).push([[306],{930:function(e,t,u){var n=u(836);t.__esModule=!0,t.default=void 0;var a=n(u(867)),r=n(u(294)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,a.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component),c=l;t.default=c}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-e24185520deeb15bc4cd.js.map
