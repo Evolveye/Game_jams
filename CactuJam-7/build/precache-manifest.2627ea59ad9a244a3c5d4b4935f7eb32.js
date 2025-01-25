cfcf5db088a7cd194c8155a88f1e0f046bc1229b
evolveye <stolarski.paw@gmail.com>
2025-01-25 23:31:59 +0100

    Fix %PUBLIC_URL% by changing "homepage" field in package.json

---
+++ CactuJam-7/build/precache-manifest.2627ea59ad9a244a3c5d4b4935f7eb32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928f28ef1e23e58225ddac54425722a0",
+    "url": "/Game_jams/CactuJam-7/build/index.html"
+  },
+  {
+    "revision": "4e57fe8bd0440f96108f",
+    "url": "/Game_jams/CactuJam-7/build/static/css/main.8ef45442.chunk.css"
+  },
+  {
+    "revision": "219dffcd81b2ae687f79",
+    "url": "/Game_jams/CactuJam-7/build/static/js/2.f4b45162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Game_jams/CactuJam-7/build/static/js/2.f4b45162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e57fe8bd0440f96108f",
+    "url": "/Game_jams/CactuJam-7/build/static/js/main.68d7e5c3.chunk.js"
+  },
+  {
+    "revision": "e006a93f0f8283286c71",
+    "url": "/Game_jams/CactuJam-7/build/static/js/runtime-main.841abd73.js"
+  }
+]);
--- CactuJam-7/build/service-worker.js
+++ CactuJam-7/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cactujam/7/precache-manifest.5c1d573f185a924a3b75ab6c51cfd39c.js"
+  "/Game_jams/CactuJam-7/build/precache-manifest.2627ea59ad9a244a3c5d4b4935f7eb32.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cactujam/7/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Game_jams/CactuJam-7/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
