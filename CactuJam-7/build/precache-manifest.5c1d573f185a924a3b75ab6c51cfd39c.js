545bbdc7fafff74a8233f5195b099ff84ad0d89c
evolveye <stolarski.paw@gmail.com>
2025-01-25 23:25:50 +0100

    Make CactuJam-7 accessible from the web

---
+++ CactuJam-7/build/precache-manifest.5c1d573f185a924a3b75ab6c51cfd39c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbbe9cc9763dc75c7f400596168b221",
+    "url": "/cactujam/7/index.html"
+  },
+  {
+    "revision": "a93ebaf7f07e1681ac56",
+    "url": "/cactujam/7/static/css/main.8ef45442.chunk.css"
+  },
+  {
+    "revision": "219dffcd81b2ae687f79",
+    "url": "/cactujam/7/static/js/2.f4b45162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cactujam/7/static/js/2.f4b45162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a93ebaf7f07e1681ac56",
+    "url": "/cactujam/7/static/js/main.abdf389e.chunk.js"
+  },
+  {
+    "revision": "c85a158f39aab4a3f65c",
+    "url": "/cactujam/7/static/js/runtime-main.8031444c.js"
+  }
+]);
